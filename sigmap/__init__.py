ba0d9d406b9835007d482459cfd95778a3993466
Goosang Yu <gsyu93@gmail.com>
2024-12-17 22:49:33 +0900

    🐛 Fix minor mistake

--- sigmap/__init__.py
+++ sigmap/__init__.py
@@ -2,4 +2,4 @@ from sigmap.sigmapred import SigmaFactor
 from sigmap.setup import readseq, fasta2df
 
 # hatch publish --user __token__ -a <your_api_key>
-__version__ = '1.1.0'
+__version__ = '1.1.1'
