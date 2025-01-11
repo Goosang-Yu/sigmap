de630e93b8319074d30080829e4cd274ac9bce9b
Goosang Yu <gsyu93@gmail.com>
2025-01-11 16:41:30 +0900

    🔖 Release v1.1.2

--- sigmap/__init__.py
+++ sigmap/__init__.py
@@ -1,5 +1,10 @@
 from sigmap.sigmapred import SigmaFactor
 from sigmap.setup import readseq, fasta2df
 
+# hatch version patch
+# or hatch version minor
+# or hatch version major
+
+# hatch build
 # hatch publish --user __token__ -a <your_api_key>
-__version__ = '1.1.1'
+__version__ = '1.1.2'
